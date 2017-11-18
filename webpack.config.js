3af06088c75480a99871f4f5eed0763511aa756b
Kamil Gebala <kamazowe@gmail.com>
2017-11-18 16:18:18 +0100

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,17 @@ module.exports= (env)=> {
                 test: /\.s?css$/,
                 use:CSSExtract.extract({
                     use:[
-                        'css-loader',
-                        'sass-loader']
+                        {loader:'css-loader',
+                        options:{
+                            sourceMap: true
+                        }
+                        },
+                        {
+                            loader:'sass-loader',
+                            options:{
+                                sourceMap:true
+                            }
+                        }]
                 })          
 //                   [
 //                    'style-loader',
@@ -34,7 +43,7 @@ module.exports= (env)=> {
         plugins:[
           CSSExtract  
         ],
-    devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction? 'source-map' : 'inline-source-map',
     devServer:{
         contentBase:path.join(__dirname,'public'),
         historyApiFallback:true
